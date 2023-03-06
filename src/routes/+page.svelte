f506d500babe00f452dd7e8cd6766accd10239e1
James Murdza <james@jamesmurdza.com>
2023-03-06 10:49:15 +0100

    Display list of football leagues using AJAX.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	async function callAPI(URL) {
+		// Call the API, wait for response, and parse response.
+		let response = await fetch(URL, settings);
+		let responseData = await response.json();
+		return responseData.response;
+	}
+
+	// Authorization needed to connect to the API.
+	const settings = {
+		"headers": {
+			"X-RapidAPI-Key": "API_KEY",
+			"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+		}
+	};
+
+	// These variables are the page state.
+	let leagues = [];
+
+	// This function is called once to load the page.
+	async function loadData() {
+		leagues = await callAPI("https://api-football-v1.p.rapidapi.com/v3/leagues?current=true")
+	}
+	onMount(loadData);
+</script>
+
+<h1>Leagues:</h1>
+<ul>
+	{#each leagues as league}
+		<li>{league.league.name}</li>
+	{/each}
+</ul>
